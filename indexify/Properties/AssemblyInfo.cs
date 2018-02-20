d9faa5caa9ebad7889348bfdac4e01c0af8eee83
James Eby <temporafugiunt@gmail.com>
2018-02-20 15:38:55 -0600

    Added the nuget pack of the tiv.elastic project.

--- indexify/Properties/AssemblyInfo.cs
+++ indexify/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("reindexify")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("indexify")]
+[assembly: AssemblyDescription("This command line tool will reindex a single or series of source Elastic indexes based on the passed index pattern into a destination index.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("reindexify")]
+[assembly: AssemblyCompany("freebyTech")]
+[assembly: AssemblyProduct("indexify")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- tiv.elastic/Properties/AssemblyInfo.cs
+++ tiv.elastic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ElasticModels")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("tiv.elastic")]
+[assembly: AssemblyDescription("trackItsValue Elastic REST Client Library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ElasticModels")]
+[assembly: AssemblyCompany("freebyTech")]
+[assembly: AssemblyProduct("tiv.elastic")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
