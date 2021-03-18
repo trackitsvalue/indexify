92b45fd8f390a181f4a4dabb5c72a900d784f6a2
Rudolph Gatt <rudolph.gatt@gig.com>
2021-03-18 21:22:38 +0100

    Added the following features: 1. Ability to create destination index with source mappings and settings 2. Ability to specify number of primary shards 3. Ability to specify number of replicas 4. Ability to parallelize re-indexing

--- tiv.elasticClient/APIs/_cat/CatAPI.cs
+++ tiv.elasticClient/APIs/_cat/CatAPI.cs
@@ -19,14 +19,33 @@ namespace tiv.elasticClient.APIs._cat
             var request = new RestRequest(resource, Method.GET) {RequestFormat = DataFormat.Json};
             request.AddHeader("Accept", "application/json");
 
-            //{
-            //    JsonSerializer = new JsonSerializer()
-            //};
-
             var response = client.Execute<List<CatResponse>>(request);
 
             if (response.IsSuccessful) return response.Data;
             throw new RestCallException(response.StatusCode, response.StatusDescription, response.ErrorMessage, response.ErrorException);
-        } 
+        }
+
+        public bool IndexExists(IRestClient client, string index)
+        {
+            index = index.TrimSlashes();
+
+            var resource = $"_cat/indices/{index}?format=json&pretty=true";
+
+            var request = new RestRequest(resource, Method.GET) { RequestFormat = DataFormat.Json };
+            request.AddHeader("Accept", "application/json");
+
+            var response = client.Execute<List<CatResponse>>(request);
+
+            if (response.IsSuccessful)
+            {
+                if (response.Data != null && response.Data.Count > 0) return true;
+
+                return false;
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return false;
+
+            throw new RestCallException(response.StatusCode, response.StatusDescription, response.ErrorMessage, response.ErrorException);
+        }
     }
 }
---
+++ tiv.elasticClient/APIs/_settings/Models/SettingsResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace tiv.elasticClient.APIs._settings.Models
+{
+    public class SettingsResponse
+    {
+        [JsonProperty("index")]
+        public SettingsIndex SettingsIndex { get; set; }
+    }
+
+    public class SettingsIndex
+    {
+        [JsonProperty("number_of_shards")]
+        public string NumberOfShards { get; set; }
+
+        [JsonProperty("number_of_replicas")]
+        public string NumberOfReplicas { get; set; }
+    }
+}
